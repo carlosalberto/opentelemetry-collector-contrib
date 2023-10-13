e623fadcaeea09bee7c7429ba3dfca954cdbe8b1
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2023-10-13 16:01:14 +0200

    Add port-in-use test.

--- receiver/lightstepreceiver/trace_receiver_test.go
+++ receiver/lightstepreceiver/trace_receiver_test.go
@@ -4,10 +4,13 @@
 package lightstepreceiver
 
 import (
+	"context"
+	"net"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/component/componenttest"
 	"go.opentelemetry.io/collector/config/confighttp"
 	"go.opentelemetry.io/collector/consumer"
 	"go.opentelemetry.io/collector/consumer/consumertest"
@@ -57,5 +60,26 @@ func TestNew(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestReceiverPortAlreadyInUse(t *testing.T) {
+	l, err := net.Listen("tcp", "localhost:")
+	require.NoError(t, err, "failed to open a port: %v", err)
+	defer l.Close()
+	_, portStr, err := net.SplitHostPort(l.Addr().String())
+	require.NoError(t, err, "failed to split listener address: %v", err)
+	cfg := &Config{
+		Protocols: Protocols{
+			HTTP: &HTTPConfig{
+				HTTPServerSettings: &confighttp.HTTPServerSettings{
+					Endpoint: "localhost:" + portStr,
+				},
+			},
+		},
+	}
+	traceReceiver, err := newReceiver(cfg, consumertest.NewNop(), receivertest.NewNopCreateSettings())
+	require.NoError(t, err, "Failed to create receiver: %v", err)
+	err = traceReceiver.Start(context.Background(), componenttest.NewNopHost())
+	require.Error(t, err)
 }
 
